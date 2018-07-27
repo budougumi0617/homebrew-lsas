4803df1dd094ed3d1a209b07079abb1db59454cf
budougumi0617 <budougumi0617@gmail.com>
2018-07-27 18:04:45 +0900

    :shirt: Remove dusts

--- lsas.rb
+++ lsas.rb
@@ -9,13 +9,6 @@ class Lsas < Formula
 
   if Hardware::CPU.is_32_bit?
     if OS.linux?
-      SHA256(dist/snapshot/lsas_linux_386.zip)= 3c047d70f37ff9b815b6d0c0268c8a42e6fdde9616e7f3885d9a4829c6c89688
-openssl dgst -sha256 dist/snapshot/lsas_darwin_386.zip
-SHA256(dist/snapshot/lsas_darwin_386.zip)= aece51c380b8d82a0a02ee8e193dd2248357a2f42adfe81c25c9c76191a46af1
-openssl dgst -sha256 dist/snapshot/lsas_darwin_amd64.zip
-SHA256(dist/snapshot/lsas_darwin_amd64.zip)= bd3c9b3ee4b8ef67e4a9b94e980014788aed5f186a888f10dd603d7d9a17510d
-openssl dgst -sha256 dist/snapshot/lsas_linux_amd64.zip
-SHA256(dist/snapshot/lsas_linux_amd64.zip)= 60a1b373d6dd98c1b56b89203bbca064d060185652dc172051595a85e6fd4490
       url "#{REPOSITORY_URL}/releases/download/#{HOMEBREW_LSAS_VERSION}/lsas_linux_386.zip"
       # openssl dgst -sha256 dist/snapshot/lsas_linux_386.zip
       sha256 '3c047d70f37ff9b815b6d0c0268c8a42e6fdde9616e7f3885d9a4829c6c89688'
